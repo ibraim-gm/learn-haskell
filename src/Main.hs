d6075a340ea0b023e19971092a6d13fdb919f0a7
Rafael Ibraim <ibraim.gm@gmail.com>
2013-09-18 08:38:32 -0300

    Added a simple State monad sample

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ import Patterns
 import Rational
 import Tree
 import RPN
+import StackDemo
 
 main = do
    putStrLn "--- Baby Steps"
@@ -11,7 +12,9 @@ main = do
    showPatterns
    putStrLn "--- Naive implementation of rational numbers"
    showRational
-   putStrLn "--- Making a funtor for a tree"
+   putStrLn "--- Making a functor for a tree"
    showTree
    putStrLn "--- Simple RPN calculator"
    showRPN
+   putStrLn "--- Stack (State) demo"
+   showStackDemo
---
+++ src/StackDemo.hs
@@ -0,0 +1,35 @@
+
+module StackDemo ( showStackDemo ) where
+
+import Control.Monad.Trans.State
+
+showStackDemo :: IO ()
+showStackDemo = putStrLn . show $ evalState manipulateStack []
+
+-- Whe define a stack as a list of ints
+type Stack = [Int]
+
+-- Now, we define a StackState, something that will keep the state of our stack
+type StatefulStack a = State Stack a
+
+-- pushing values to the stack
+push :: Int -> StatefulStack ()
+push a = state $ \xs -> ((),a:xs)
+
+-- pop values from the state
+pop :: StatefulStack Int
+pop = state $ \(x:xs) -> (x,xs)
+
+-- peek the last value
+peek :: StatefulStack Int
+peek = state $ \(x:xs) -> (x,x:xs)
+
+-- manipulate the stack
+manipulateStack :: StatefulStack Int
+manipulateStack = do
+  push 5
+  push 7
+  a <- pop
+  b <- pop
+  push (a + b)
+  peek
